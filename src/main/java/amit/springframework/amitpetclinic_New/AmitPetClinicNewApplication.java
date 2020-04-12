ed41da95f497d0577377e26745dfab017c7dbe8c
amitmodi06 <amitmodi06@gmail.com>
2020-04-12 19:00:33 +0530

    Initial project commit to Git

---
+++ src/main/java/amit/springframework/amitpetclinic_New/AmitPetClinicNewApplication.java
@@ -0,0 +1,13 @@
+package amit.springframework.amitpetclinic_New;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmitPetClinicNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmitPetClinicNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amit/springframework/amitpetclinic_New/AmitPetClinicNewApplicationTests.java
@@ -0,0 +1,13 @@
+package amit.springframework.amitpetclinic_New;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmitPetClinicNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
