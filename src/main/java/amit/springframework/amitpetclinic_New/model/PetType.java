4df0ac20b4e541efecf8437676a19d78945d246e
amitmodi06 <amitmodi06@gmail.com>
2020-04-16 23:46:17 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/amit/springframework/amitpetclinic_New/model/PetType.java
@@ -0,0 +1,16 @@
+package amit.springframework.amitpetclinic_New.model;
+
+/**
+ * created by KUAM on 4/16/2020
+ */
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
