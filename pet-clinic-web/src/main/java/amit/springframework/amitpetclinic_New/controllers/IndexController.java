219b3b422b5390f5e22fa7366b79b5cfce01d26f
amitmodi06 <amitmodi06@gmail.com>
2020-04-19 23:04:33 +0530

    Adding Index page and controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/amit/springframework/amitpetclinic_New/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package amit.springframework.amitpetclinic_New.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by KUAM on 4/19/2020
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
