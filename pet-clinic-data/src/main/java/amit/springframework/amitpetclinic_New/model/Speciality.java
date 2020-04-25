129d44748597a3d1469e0ced6d616f578776276e
amitmodi06 <amitmodi06@gmail.com>
2020-04-25 22:32:42 +0530

    Added Specialty entity and added speciality to Vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Speciality.java
@@ -0,0 +1,17 @@
+package amit.springframework.amitpetclinic_New.model;
+
+/**
+ * created by KUAM on 4/25/2020
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Vet.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Vet.java
@@ -1,7 +1,19 @@
 package amit.springframework.amitpetclinic_New.model;
 
+import java.util.Set;
+
 /**
  * created by KUAM on 4/16/2020
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
