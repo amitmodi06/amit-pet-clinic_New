75abd56c595aeee4fb91202d4a8333ca7f7ed08e
amitmodi06 <amitmodi06@gmail.com>
2020-04-25 23:01:23 +0530

    Correcting typo mistake.

--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/services/PetTypeService.java
@@ -1,7 +1,9 @@
 package amit.springframework.amitpetclinic_New.services;
 
+import amit.springframework.amitpetclinic_New.model.PetType;
+
 /**
  * created by KUAM on 4/25/2020
  */
-public interface PetTypeService extends CrudService<PetTypeService, Long> {
+public interface PetTypeService extends CrudService<PetType, Long> {
 }
