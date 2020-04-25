ad49747e33c29e8ab5a290d4c13c4ef95b94a8a3
amitmodi06 <amitmodi06@gmail.com>
2020-04-25 22:22:29 +0530

    Added Visit and updated Owner with Pet relationship.  Closes #26

--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Owner.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Owner.java
@@ -1,7 +1,19 @@
 package amit.springframework.amitpetclinic_New.model;
 
+import java.util.Set;
+
 /**
  * created by KUAM on 4/16/2020
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Visit.java
@@ -0,0 +1,37 @@
+package amit.springframework.amitpetclinic_New.model;
+
+import java.time.LocalDate;
+
+/**
+ * created by KUAM on 4/25/2020
+ */
+public class Visit extends BaseEntity{
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
