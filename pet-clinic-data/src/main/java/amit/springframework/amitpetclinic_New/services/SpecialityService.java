8750c023258b907e42b293ccef45ae4ac6c67a95
amitmodi06 <amitmodi06@gmail.com>
2020-04-26 10:21:23 +0530

    Adding Specialties Map service impl. Closes #31

---
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package amit.springframework.amitpetclinic_New.services;
+
+import amit.springframework.amitpetclinic_New.model.Speciality;
+
+/**
+ * created by KUAM on 4/26/2020
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/services/map/SpecialityServiceMap.java
@@ -0,0 +1,39 @@
+package amit.springframework.amitpetclinic_New.services.map;
+
+import amit.springframework.amitpetclinic_New.model.Speciality;
+import amit.springframework.amitpetclinic_New.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * created by KUAM on 4/26/2020
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
