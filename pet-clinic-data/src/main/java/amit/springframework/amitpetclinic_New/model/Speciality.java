74d76c7686d94a8629724ba2b4f0237b84b6e977
amitmodi06 <amitmodi06@gmail.com>
2020-05-01 17:07:18 +0530

    Updated Vet to use the JPA entity. Closes #37

--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Speciality.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Speciality.java
@@ -1,10 +1,17 @@
 package amit.springframework.amitpetclinic_New.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * created by KUAM on 4/25/2020
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Vet.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Vet.java
@@ -1,13 +1,19 @@
 package amit.springframework.amitpetclinic_New.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * created by KUAM on 4/16/2020
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
