73652d6d85feb1f140127a07f8e0281cf68c375b
amitmodi06 <amitmodi06@gmail.com>
2020-05-01 22:31:29 +0530

    Changing Line Separator to LF

--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Person.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/Person.java
@@ -1,32 +1,1 @@
-package amit.springframework.amitpetclinic_New.model;
-
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-
-/**
- * created by KUAM on 4/16/2020
- */
-@MappedSuperclass
-public class Person extends BaseEntity {
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-}
+package amit.springframework.amitpetclinic_New.model;import javax.persistence.Column;import javax.persistence.MappedSuperclass;/** * created by KUAM on 4/16/2020 */@MappedSuperclasspublic class Person extends BaseEntity {    @Column(name = "first_name")    private String firstName;    @Column(name = "last_name")    private String lastName;    public String getFirstName() {        return firstName;    }    public void setFirstName(String firstName) {        this.firstName = firstName;    }    public String getLastName() {        return lastName;    }    public void setLastName(String lastName) {        this.lastName = lastName;    }}
