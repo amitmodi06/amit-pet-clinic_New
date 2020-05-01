76b06074a1012530d152adc599b71fefd932e526
amitmodi06 <amitmodi06@gmail.com>
2020-05-01 23:23:25 +0530

    Created Spring Data JPA service for Speciality. Closes #44.

---
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/services/springdatajpa/SpecialityServiceSDJpa.java
@@ -0,0 +1,51 @@
+package amit.springframework.amitpetclinic_New.services.springdatajpa;
+
+import amit.springframework.amitpetclinic_New.model.Speciality;
+import amit.springframework.amitpetclinic_New.repositories.SpecialityRepository;
+import amit.springframework.amitpetclinic_New.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * created by KUAM on 5/1/2020
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialityServiceSDJpa implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityServiceSDJpa(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
