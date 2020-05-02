ab7425f466a961db9fcfc3f717163a32757fc1d3
amitmodi06 <amitmodi06@gmail.com>
2020-05-02 17:24:34 +0530

    Added Project Lombok to the project. Closes #51.

--- pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/PetType.java
+++ pet-clinic-data/src/main/java/amit/springframework/amitpetclinic_New/model/PetType.java
@@ -1,5 +1,7 @@
 package amit.springframework.amitpetclinic_New.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,6 +9,11 @@ import javax.persistence.Table;
 /**
  * created by KUAM on 4/16/2020
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
@@ -14,11 +21,4 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
