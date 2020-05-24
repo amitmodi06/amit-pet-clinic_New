1079c03bce7594b3885cbb44ab3f93ae561208f7
amitmodi06 <amitmodi06@gmail.com>
2020-05-24 19:00:08 +0530

    Added Model Attribute for Pet Controller(using @ModelAttribute)

---
+++ pet-clinic-web/src/main/java/amit/springframework/amitpetclinic_New/controllers/PetController.java
@@ -0,0 +1,50 @@
+package amit.springframework.amitpetclinic_New.controllers;
+
+import amit.springframework.amitpetclinic_New.model.Owner;
+import amit.springframework.amitpetclinic_New.model.PetType;
+import amit.springframework.amitpetclinic_New.services.OwnerService;
+import amit.springframework.amitpetclinic_New.services.PetService;
+import amit.springframework.amitpetclinic_New.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+/**
+ * created by KUAM on 5/24/2020
+ */
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetType(){
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId){
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+}
