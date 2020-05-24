cc4ceee991143d5c6db5c4a2821ecbb0a1e4705f
amitmodi06 <amitmodi06@gmail.com>
2020-05-25 00:17:06 +0530

    Fixed Parsing issue in pettype. Closes #66

---
+++ pet-clinic-web/src/main/java/amit/springframework/amitpetclinic_New/formatters/PetTypeFormatter.java
@@ -0,0 +1,41 @@
+package amit.springframework.amitpetclinic_New.formatters;
+
+import amit.springframework.amitpetclinic_New.model.PetType;
+import amit.springframework.amitpetclinic_New.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * created by KUAM on 5/25/2020
+ */
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
