3bdd28edcbb15ba468340fea54cccc1f0c7adc63
amitmodi06 <amitmodi06@gmail.com>
2020-05-30 21:53:49 +0530

    Implemented Vets JSON end point. Closes #69.

--- pet-clinic-web/src/main/java/amit/springframework/amitpetclinic_New/controllers/VetController.java
+++ pet-clinic-web/src/main/java/amit/springframework/amitpetclinic_New/controllers/VetController.java
@@ -1,9 +1,14 @@
 package amit.springframework.amitpetclinic_New.controllers;
 
+import amit.springframework.amitpetclinic_New.model.Vet;
 import amit.springframework.amitpetclinic_New.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * created by KUAM on 4/19/2020
@@ -22,5 +27,11 @@ public class VetController {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
